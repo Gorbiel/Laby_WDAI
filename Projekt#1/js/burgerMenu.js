ce20dbc27ae14e13c8f7dd9c7e595a60dd5933af
Gorbiel <gracz1212yt@gmail.com>
2024-10-29 15:42:27 +0100

    added burger style menu for more responsive experience

---
+++ Projekt#1/js/burgerMenu.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const burgerMenu = document.querySelector('.burger-menu');
+    const menu = document.getElementById('menu');
+
+    burgerMenu.addEventListener('click', () => {
+        menu.classList.toggle('show');
+    });
+});
