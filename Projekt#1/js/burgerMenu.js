87a0a1a83bcd97f380be3e7f9d66e0e6aa0733d5
Gorbiel <gracz1212yt@gmail.com>
2024-11-14 10:25:28 +0100

    init

--- Projekt#1/js/burgerMenu.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const burgerMenu = document.querySelector('.burger-menu');
-    const menu = document.getElementById('menu');
-
-    burgerMenu.addEventListener('click', () => {
-        menu.classList.toggle('show');
-    });
-});
-
